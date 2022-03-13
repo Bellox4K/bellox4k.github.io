f3e49d9df7abbf86ef8c7ce94555579035237a75
Bellox <billiabcd@gmail.com>
2022-03-13 16:29:12 +0100

    real

--- what.js
+++ what.js
@@ -1,11 +1,3 @@
 window.onload = function(){
-
-document.querySelector('#no').addEventListener("click" , function(){
-    document.querySelector('#confirm').style.opacity = '0';
-})
-
-document.querySelector('#yes').addEventListener("click" , function(){
-    document.querySelector('#confirm').style.opacity = '0';
     document.querySelector("audio").play();
-})
 }
