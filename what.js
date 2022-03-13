e33ad70199b39d2b8c49f6c8cf3b6a11aeed96f9
Bellox <billiabcd@gmail.com>
2022-03-13 16:33:50 +0100

    Update what.js

--- what.js
+++ what.js
@@ -2,7 +2,6 @@ window.onload = function(){
 
 document.querySelector('#no').addEventListener("click" , function(){
     document.querySelector('#confirm').style.opacity = '0';
-    document.querySelector("audio").play();
 })
 
 document.querySelector('#yes').addEventListener("click" , function(){
