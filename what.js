9b74796743a713ea3d626eb86e0df76a95168f23
Bellox <billiabcd@gmail.com>
2022-03-13 16:47:48 +0100

    the guts

--- what.js
+++ what.js
@@ -9,3 +9,8 @@ document.querySelector('#yes').addEventListener("click" , function(){
     document.querySelector("audio").play();
 })
 }
+        function picture(){ 
+        var pic = "https://pbs.twimg.com/media/FMVdxo8VkAAfn3N?format=jpg&name=900x900"
+        document.getElementById('bigpic').src = pic.replace('90x90', '225x225');
+
+        }
