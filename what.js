b0d2557262bc423c376952a3573c3b98a2ede91c
Bellox <billiabcd@gmail.com>
2022-03-13 16:21:23 +0100

    added

---
+++ what.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+
+document.querySelector('#no').addEventListener("click" , function(){
+    document.querySelector('#confirm').style.opacity = '0';
+})
+
+document.querySelector('#yes').addEventListener("click" , function(){
+    document.querySelector('#confirm').style.opacity = '0';
+    document.querySelector("audio").play();
+})
+}
